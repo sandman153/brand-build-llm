647150f0f11465af548fb6ea0932107742023840
Sarath Boppudi <26595971+sandman153@users.noreply.github.com>
2024-08-06 21:51:38 +1000

    updated workflow

--- iac-terraform/environments/prod/providers.tf
+++ iac-terraform/environments/prod/providers.tf
@@ -1,9 +1,5 @@
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = data.local_file.sa_key.content
-}
-
-data "local_file" "sa_key" {
-  filename = var.credentials_file_path
+  credentials = file(var.credentials_file_path)
 }
