69c252824a0f3f3ae2da91cb6be5ea21e0c70598
Sarath Boppudi <26595971+sandman153@users.noreply.github.com>
2024-08-11 20:10:23 +1000

    updated workflow - 202408112005

--- iac-terraform/modules/cloud_functions/main.tf
+++ iac-terraform/modules/cloud_functions/main.tf
@@ -2,16 +2,21 @@ resource "google_cloudfunctions_function" "gemini_function" {
     name = "geminiFunction"
     runtime = "python39"
     entry_point = "main"
-    source_repository {
-        url = var.repository_url
-    }
-    event_trigger {
-        event_type = "google.pubsub.topic.publish"
-        resource = var.pubsub_topic
-    }
+    region = var.region
+
+    # Following set to empty values as they will be deployed later
+    source_archive_bucket = "" # Remove this line entirely during initial setup
+    source_archive_object = "" # Remove this line entirely during initial setup
+
+    trigger_http = true
 
     environment_variables = {
       PROJECT_ID = var.project_id
+      "ENV" = "production"
     }
+
+    # Set memory, timeout, etc
+    available_memory_mb = 256
+    timeout = 60
   
 }
